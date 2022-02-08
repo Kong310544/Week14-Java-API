980798da8115a51aeace3d85e5d0bdc72cc9087e
Kong310544 <kong310544@gmail.com>
2022-02-08 14:14:45 +0700

    kkkkk

--- src/main/java/nvc/it/springapi/model/Product.java
+++ src/main/java/nvc/it/springapi/model/Product.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -35,7 +36,7 @@ public class Product {
     private Date createdAt;
 
 
-    @LastModifiedBy
+    @LastModifiedDate
     private Date updatedAt;
 
     
